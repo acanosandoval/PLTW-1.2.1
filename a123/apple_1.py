c0d7075705eafbc97eaae36af493320a0d4bd6cc
acanosandoval <114962747+acanosandoval@users.noreply.github.com>
2022-12-04 05:18:37 -0500

    .....

--- a123/apple_1.py
+++ a123/apple_1.py
@@ -1,5 +1,7 @@
 #   a123_apple_1.py
 import turtle as trtl
+import random as rand
+rand.randint()
 
 #-----setup-----
 apple_image = "a123/apple.gif" # Store the file name of your shape
@@ -11,9 +13,19 @@ wn.addshape(apple_image) # Make the screen aware of the new file
 
 apple = trtl.Turtle()
 apple.penup()
-
 xcor = apple.xcor()
 ycor = apple.ycor()
+
+letters_list = ["a", "s", "d", "f", "g", "h", "i", "j", "k", "l"]
+letter = letters_list.pop()
+
+# letter_instruct
+letter_instruct = trtl.Turtle()
+letter_instruct.hideturtle()
+letter_instruct.penup()
+letter_instruct.goto(-20, 100)
+letter_instruct.color("white")
+letter_instruct.write(letter, font=("Arial", 50))
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
@@ -21,13 +33,15 @@ def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
 
-def drop_apple(active_apple):
-  active_apple.setheading(270)
-  active_apple.forward(140)
+# drops apple from tree
+def drop_apple():
+  apple.goto(0, -160)
+  letter_instruct.clear()
+  apple.hideturtle()
 
 #-----function calls-----
 draw_apple(apple)
 
-wn.onkeypress(drop_apple, "a")
-
+wn.onkeypress(drop_apple, letter)
+wn.listen()
 wn.mainloop()
