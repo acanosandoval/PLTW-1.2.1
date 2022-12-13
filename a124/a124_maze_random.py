7fdc6eae507ae1cff17f294696ebfcaf4ee8caee
acanosandoval <114962747+acanosandoval@users.noreply.github.com>
2022-12-13 09:19:35 -0500

    wdgf

--- a124/a124_maze_random.py
+++ a124/a124_maze_random.py
@@ -6,7 +6,7 @@ wn = trtl.Screen()
 
 #----configuration variables setup----
 num_of_walls = 5 * 5
-wall_length = 10
+wall_length = 50
 path_width = 12
 wall_color = "black"
 wall_count = 1
@@ -29,24 +29,43 @@ for r in range(num_of_walls):
     wall_pen.pendown()
   else:
     wall_pen.penup()
-    
-  if door < barrier:
-    wall_pen.left(90)
+  
+  wall_pen.left(90)
+
+  if (door < barrier):
+    print("door < barrier")
     wall_pen.forward(door)
     wall_pen.penup() #---draws doors---
-    wall_pen.forward(path_width)
+    wall_pen.forward(path_width*2)
     if wall_count > 4: #cont. of 4 wall skip
       wall_pen.pendown()
     #---draws barriers---
-    wall_pen.forward(barrier - door)
+    wall_pen.forward(barrier - door - path_width*2)
     wall_pen.left(90)
     wall_pen.forward(path_width*2)
     wall_pen.back(path_width*2)
     wall_pen.right(90)
-    wall_pen.forward(wall_length)
+    wall_pen.forward(wall_length - barrier)
+
     wall_length = wall_length + path_width
     wall_count = wall_count + 1
   else:
-    wall_count = 0
+    print("door > barrier")
+    wall_pen.forward(barrier)
+    wall_pen.left(90)
+    wall_pen.forward(path_width*2)
+    wall_pen.back(path_width*2)
+    wall_pen.right(90)
+    
+    wall_pen.forward(door - barrier)
+    wall_pen.penup() #---draws doors---
+    wall_pen.forward(path_width*2)
+    if wall_count > 4: #cont. of 4 wall skip
+      wall_pen.pendown()
+    
+    wall_pen.forward(wall_length - door - path_width*2)
+
+    wall_length = wall_length + path_width
+    wall_count = wall_count + 1
 
 wn.mainloop()
