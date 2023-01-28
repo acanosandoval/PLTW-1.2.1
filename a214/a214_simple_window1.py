66b71ccaf560032cddf90b5358ce7f4ffd9db53d
acanosandoval <114962747+acanosandoval@users.noreply.github.com>
2023-01-28 07:46:51 -0500

    h

---
+++ a214/a214_simple_window1.py
@@ -0,0 +1,19 @@
+#   a214_simple_window1.py
+#   A program creates a window on your screen using Tkinter.
+import tkinter as tk
+
+# main window
+root = tk.Tk()
+root.wm_geometry("200x100")
+root.title('Authorization')
+
+# create empty frame
+frame_login = tk.Frame(root)
+frame_login.grid()
+
+lbl_username = tk.Label(frame_login, text='Username:')
+lbl_username.pack()
+
+tk.Label(frame_login,text="Password:",font="Courier")
+
+root.mainloop()
