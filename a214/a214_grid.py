5ea5b73c8ad965b3b3298be79f28d4074dfd8358
acanosandoval <114962747+acanosandoval@users.noreply.github.com>
2023-01-29 01:47:39 -0500

    up in flames

---
+++ a214/a214_grid.py
@@ -0,0 +1,26 @@
+#   a214_simple_window1.py
+#   A program creates a window on your screen using Tkinter.
+import tkinter as tk
+
+#main window
+root = tk.Tk()
+root.wm_geometry("300x300")
+root.title("Grid")
+
+#BLUE FRAME
+blue_frame = tk.Frame(root, background='blue', width=200, height=150)
+blue_frame.grid(row=0, column=0)
+
+#GREEN FRAME
+green_frame = tk.Frame(root, background='green', width=100, height=150)
+green_frame.grid(row=0, column=1)
+
+#RED FRAME
+red_frame = tk.Frame(root, background='red', width=200, height=150)
+red_frame.grid(row=1, column=0)
+
+#YELLOW FRAME
+red_frame = tk.Frame(root, background='yellow', width=100, height=150)
+red_frame.grid(row=1, column=1)
+
+root.mainloop()
--- a214/a214_simple_window1.py
+++ a214/a214_simple_window1.py
@@ -2,18 +2,51 @@
 #   A program creates a window on your screen using Tkinter.
 import tkinter as tk
 
-# main window
+#button testing func
+def test_my_button():
+    frame_auth.tkraise()
+    password_display = ent_password.get()
+    lbl_password_show.config(text=password_display)
+
+#main window
 root = tk.Tk()
-root.wm_geometry("200x100")
-root.title('Authorization')
+root.wm_geometry("200x160")
+root.title("Authorization")
 
-# create empty frame
+#LOGIN FRAME
 frame_login = tk.Frame(root)
-frame_login.grid()
+frame_login.grid(row=0, column=0, sticky="news")
 
-lbl_username = tk.Label(frame_login, text='Username:')
+#username label
+lbl_username = tk.Label(frame_login, text='Username:',font="Courier")
 lbl_username.pack()
+#username entry box
+ent_username = tk.Entry(frame_login, bd=3)
+ent_username.pack(pady=5)
 
-tk.Label(frame_login,text="Password:",font="Courier")
+#password label
+lbl_password = tk.Label(frame_login,text="Password:",font="Courier")
+lbl_password.pack()
+#password entry box
+ent_password = tk.Entry(frame_login, bd=3, show='*')
+ent_password.pack(pady=5)
+
+#login button
+btn_login = tk.Button(frame_login, text='Login', command=test_my_button)
+btn_login.pack(pady=5)
+
+
+
+#AUTHORIZATION FRAME
+frame_auth = tk.Frame(root)
+frame_auth.grid(row=0, column=0, sticky="news")
+
+#password shown label
+lbl_password_show = tk.Label(frame_auth,font="Courier")
+lbl_password_show.pack()
+
+
+#(raises login frame above auth frame)
+frame_login.tkraise()
 
 root.mainloop()
