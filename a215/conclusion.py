fb903b9e66d5d73273f71a177e27c808d0b3513a
acanosandoval <114962747+acanosandoval@users.noreply.github.com>
2023-02-05 10:15:08 -0500

    what if

---
+++ a215/conclusion.py
@@ -0,0 +1,10 @@
+# a215_multi_factor.py
+import tkinter as tk
+
+my_str = ""
+
+if (len(my_str) == 0):
+  print("did it")
+  
+if (my_str!= "password"):
+  print("did it")
--- a215/num16/multi_factor.py
+++ a215/num16/multi_factor.py
@@ -3,17 +3,14 @@ import tkinter as tk
 import multifactorgui as mfg
 
 ent_user = input("Enter your username: " )
-if (len(ent_user)<8) or (len(ent_user)>24):
-    print("Invalid username; must be 8-24 characters long.")
+while (len(ent_user)<8) or (len(ent_user)>24) or ent_user.isalpha() or ent_user.isdigit():
+    print("Username must be 8-24 characters long and include both letters and numbers.")
     ent_user = input("Enter a new username: " )
-
-elif not ent_user.isalpha():
-    print("Invalid username; must include both letters and numbers.")
-    ent_user = input("Enter a new username: " )
-    
-print("YYYYEA")
 
 ent_password = input("Enter your password: " )
+while (len(ent_password)<8) or (len(ent_password)>24) or ent_password.isalpha() or ent_password.isdigit():
+    print("Password must be 8-24 characters long and include both letters and numbers.")
+    ent_password = input("Enter a new password: " )
 
 # create a multi-factor interface to a restircted app
 my_auth = mfg.MultiFactorAuth()
